44794d7b6e932f54272e10550031c3503528e554
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-01-10 08:29:06 -0800

    summry card component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import About from "./components/About";
+import SummaryCard from "./components/SummaryCard";
 function App() {
   return (
     <div className="App">
       <About />
+      <SummaryCard/>
     </div>
   );
 }
