5ee4f319a2627cd30d0947dc1f1275ce5fff96a7
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-01-11 22:12:23 -0800

    navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+import { Link, NavLink, withRouter } from "react-router-dom";
+
+const Navbar = (props) => {
+  return (
+    <nav className="nav-wrapper red darken-3">
+      <div className="container">
+        <Link className="brand-logo" to="/">
+          Poke' Time
+        </Link>
+        <ul className="right">
+          <li>
+            <NavLink exact to="/">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink exact to="/">
+              About
+            </NavLink>
+          </li>
+          <li>
+            <NavLink exact to="/contact">
+              Contact
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default withRouter(Navbar);
